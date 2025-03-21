cb47c810e7b98c8036a4bc79bbf6b811762b172a
FR <m.fslr13+gh@gmail.com>
2025-03-21 11:55:23 +0000

    initializing project

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	x "beginnerGo/exercise"
+	lib "beginnerGo/lib"
+)
+
+func main() {
+	lib.BasicGo()
+	lib.ControlFlow()
+	lib.Iteration()
+	lib.BasicDataStructures()
+	lib.AdvancedDS()
+	lib.Pointers()
+	lib.Exercise()
+	x.Exercise()
+
+}
